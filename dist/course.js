1a17d17031040a58e9b7da6844afa94864cd94cf
Ryker Blunck <ryker.blunck@atomicjolt.com>
2024-10-17 11:16:08 -0600

    fix other PDF download buttons

--- dist/course.js
+++ dist/course.js
@@ -14,11 +14,23 @@
     const button = document.querySelector(".download-button-container .button-wrapper .download-file");
     if (!button) return;
     let href = button.getAttribute("href");
-    if (href && href.startsWith("./static_resources")) {
+    if (href.startsWith("./static_resources")) {
       href = href.replace(/^\.\//, "../../");
+    }
+    href = href + "?forcedownload=true";
+    button.setAttribute("href", href);
+  }
+  function overridePdfThumbnailDownload() {
+    const buttons = document.querySelectorAll(".resource-thumbnail[href$='.pdf']");
+    buttons.forEach((button) => {
+      let href = button.getAttribute("href");
+      if (href.startsWith("./static_resources")) {
+        href = href.replace(/^\.\//, "../../");
+      }
       href = href + "?forcedownload=true";
       button.setAttribute("href", href);
-    }
+      button.removeAttribute("download");
+    });
   }
   async function injectOfflineVideos() {
     console.log(await caches.keys());
@@ -42,6 +54,7 @@
   function init() {
     overrideHomeButton();
     overridePdfDownload();
+    overridePdfThumbnailDownload();
     injectOfflineVideos();
   }
   init();
--- src/course.ts
+++ src/course.ts
@@ -13,12 +13,26 @@ function overridePdfDownload() {
   const button = document.querySelector('.download-button-container .button-wrapper .download-file')
   if (!button) return;
 
-  let href = button.getAttribute('href');
-  if (href && href.startsWith('./static_resources')) {
+  let href = button.getAttribute('href')!
+  if (href.startsWith('./static_resources')) {
     href = href.replace(/^\.\//, '../../');
+  }
+
+  href = href + '?forcedownload=true';
+  button.setAttribute('href', href);
+}
+
+function overridePdfThumbnailDownload() {
+  const buttons = document.querySelectorAll(".resource-thumbnail[href$='.pdf']");
+  buttons.forEach(button => {
+    let href = button.getAttribute('href')!;
+    if (href.startsWith('./static_resources')) {
+      href = href.replace(/^\.\//, '../../');
+    }
     href = href + '?forcedownload=true';
     button.setAttribute('href', href);
-  }
+    button.removeAttribute('download');
+  });
 }
 
 async function injectOfflineVideos() {
@@ -52,6 +66,7 @@ async function injectOfflineVideos() {
 function init() {
   overrideHomeButton()
   overridePdfDownload()
+  overridePdfThumbnailDownload()
   injectOfflineVideos()
 }
 
