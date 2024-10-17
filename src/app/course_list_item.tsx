beb817dffd899853ac9d6c5dc7229bb8e1c9b7cc
Ryker Blunck <ryker.blunck@atomicjolt.com>
2024-10-17 16:55:02 -0600

    fix image path and move SVGs out

--- src/app/course_list_item.tsx
+++ src/app/course_list_item.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'wouter';
 import { Course } from '../types';
+import { Checkmark, Download, Loader, Trash } from './svgs';
 
 interface Props {
   course: Course
@@ -24,7 +25,7 @@ export default function CourseListItem({ course, downloadCourse }: Props) {
   return (
     <>
       <a className="course-card__img" href="" aria-hidden tabIndex={-1}>
-        <img loading="lazy" src="images/creole.jp" alt="alt text" />
+        <img loading="lazy" src="images/creole.jpg" alt="alt text" />
       </a>
       <div className="course-card__content">
         <p className="u-all-caps">6.100L | Undergraduate</p>
@@ -37,7 +38,7 @@ export default function CourseListItem({ course, downloadCourse }: Props) {
       <div className="course-card__actions">
         {!course.ready 
           ? (<button onClick={beginDownload} className="btn--has-icon">
-              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2v9.67z"/></svg>Course
+              <Download />Course
             </button>)
           : (
             <Link href={`/courses/${course.id}`}>View Course</Link>
@@ -46,17 +47,17 @@ export default function CourseListItem({ course, downloadCourse }: Props) {
         {course.status && ` - ${course.status}`}
         {course.videos.length > 0 && (
           <button onClick={downloadVideos} className="btn--has-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 12v7H5v-7H3v7c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-7h-2zm-6 .67l2.59-2.58L17 11.5l-5 5-5-5 1.41-1.41L11 12.67V3h2v9.67z"/></svg>{course.videos.length} Videos
+            <Download />{course.videos.length} Videos
           </button>
         )}
         <button className="btn--has-icon is-downloading" disabled>
-            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"><path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q17 0 28.5 11.5T520-840q0 17-11.5 28.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-17 11.5-28.5T840-520q17 0 28.5 11.5T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Z"/></svg>{course.videosDownloaded}/{course.videos.length} Videos
+            <Loader />{course.videosDownloaded}/{course.videos.length} Videos
           </button>
         <button className="btn--has-icon is-success" disabled>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>{course.videos.length} Videos
+            <Checkmark />{course.videos.length} Videos
           </button>
         <button onClick={downloadVideos} className="icon-btn">
-          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>
+          <Trash />
         </button>
       </div>
     </>
