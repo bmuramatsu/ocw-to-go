58e8895ca35e3e7364c3058f111ad79d4517e772
Ryker Blunck <ryker.blunck@atomicjolt.com>
2024-11-07 14:46:09 -0700

    remove goBack action since we can just use a regular link

--- src/course/home_buttons.ts
+++
@@ -1,9 +0,0 @@
-// Make the home button go back to the PWA home page
-export default function overrideHomeButton() {
-  document.querySelectorAll("[href='https://ocw.mit.edu/']").forEach((el) => {
-    el.addEventListener("click", (e) => {
-      e.preventDefault();
-      window.parent.postMessage({ type: "goBack" });
-    });
-  });
-}
