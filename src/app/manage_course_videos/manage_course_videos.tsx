39e9909c73779610b35175e43d48cd5389f62487
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-04-21 15:33:32 -0600

    group videos on downloads page

--- src/app/manage_course_videos/manage_course_videos.tsx
+++ src/app/manage_course_videos/manage_course_videos.tsx
@@ -3,6 +3,7 @@ import { COURSES_BY_ID } from "../initial_course_list";
 import CourseVideo from "./course_video";
 import DownloadAllVideos from "./download_all_videos";
 import CourseVideoUsage from "../course_video_usage";
+import { VideoData } from "../../types";
 
 interface Props {
   courseId: string;
@@ -12,25 +13,46 @@ interface Props {
 export default function ManageCourseVideos({ courseId }: Props) {
   const courseData = COURSES_BY_ID[courseId];
 
+  const videoGroups = React.useMemo(() => {
+    const groups: Map<string, VideoData[]> = new Map();
+
+    courseData.videos.forEach((video) => {
+      const category = video.category;
+
+      if (groups.get(category)) {
+        groups.get(category)!.push(video);
+      } else {
+        groups.set(category, [video]);
+      }
+    });
+    return groups;
+  }, [courseData]);
+
   return (
     <main>
       <div className="flex wrap flex-end">
         <h1 className="h1 flex-1 u-min-300">{courseData.name}: Videos</h1>
         <div className="flex align-end flex-column">
           <DownloadAllVideos courseId={courseId} />
-          <p className="text"><CourseVideoUsage course={courseData}/></p>
+          <p className="text">
+            <CourseVideoUsage course={courseData} />
+          </p>
         </div>
       </div>
-      <h2 className="video-header">Lecture Videos</h2>
-      <div className="video-list u-mt-12">
-        {courseData.videos.map((video) => (
-          <CourseVideo
-            key={video.youtubeKey}
-            courseId={courseId}
-            video={video}
-          />
-        ))}
-      </div>
+      {Array.from(videoGroups).map(([category, videos]) => (
+        <React.Fragment key={category}>
+          <h2 className="video-header">{category}</h2>
+          <div className="video-list u-mt-12">
+            {videos.map((video) => (
+              <CourseVideo
+                key={video.youtubeKey}
+                courseId={courseId}
+                video={video}
+              />
+            ))}
+          </div>
+        </React.Fragment>
+      ))}
     </main>
   );
 }
