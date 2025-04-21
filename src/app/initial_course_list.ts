3bd40ecc3f301b72f6833e75b7e55560be19d8eb
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-04-21 17:16:12 -0600

    add more docs about course import

--- src/app/initial_course_list.ts
+++ src/app/initial_course_list.ts
@@ -3,9 +3,13 @@ import { CourseData } from "../types";
 import { RAW_COURSES } from "../courses";
 
 
+// Transforms the courses as they appear in the JSON files into
+// a more convenient format for the application.
 export const ALL_COURSES: CourseData[] = RAW_COURSES.map((course) => {
   return {
     ...course,
+    // flatten video list and add the category to each video, this is
+    // more convenient for the application to use
     videos: course.videoGroups.flatMap((group) =>
       group.videos.map((video) => ({ ...video, category: group.category })),
     ),
--- src/courses/index.ts
+++ src/courses/index.ts
@@ -1,9 +1,4 @@
 import { CourseData, VideoData } from "../types";
-import introCS from "./introduction-to-cs-and-programming-using-python.json";
-import linearAlg from "./linear-algebra.json";
-import creole from "./creole-languages-and-caribbean-identities.json";
-import introCS2 from "./introduction-to-computer-science-and-programming-in-python.json";
-
 type RawCourse = Omit<CourseData, "videos"> & {
   videoGroups: VideoGroup[];
 };
@@ -14,6 +9,13 @@ type VideoGroup = {
   category: string;
   videos: RawVideo[];
 };
+
+// To add new courses, add an import statement to the end of this block,
+// then add the course to the `RAW_COURSES` array below.
+import introCS from "./introduction-to-cs-and-programming-using-python.json";
+import linearAlg from "./linear-algebra.json";
+import creole from "./creole-languages-and-caribbean-identities.json";
+import introCS2 from "./introduction-to-computer-science-and-programming-in-python.json";
 
 // This is the order courses will be displayed in the UI
 export const RAW_COURSES: RawCourse[] = [introCS, linearAlg, introCS2, creole];
