89ee7dfc64af3e89523a49e66f52491cd881366c
Brandon Muramatsu <mura@Brandons-14-MPB.local>
2025-05-15 15:45:30 -0400

    Adding 18.01SC Calculus

--- src/courses/index.ts
+++ src/courses/index.ts
@@ -16,6 +16,7 @@ import introCS from "./introduction-to-cs-and-programming-using-python.json";
 import linearAlg from "./linear-algebra.json";
 import creole from "./creole-languages-and-caribbean-identities.json";
 import introCS2 from "./introduction-to-computer-science-and-programming-in-python.json";
+import singlevariablecalculus from "./single-variable-calculus.json";
 
 // This is the order courses will be displayed in the UI
-export const RAW_COURSES: RawCourse[] = [introCS, linearAlg, introCS2, creole];
+export const RAW_COURSES: RawCourse[] = [introCS, linearAlg, introCS2, creole, singlevariablecalculus];
