cd48a171ad2347268e11a2894462ae6a803d47e6
Brandon Muramatsu <mura@Brandons-14-MPB.local>
2025-05-15 15:53:48 -0400

    added formatting to the index.ts

--- src/courses/index.ts
+++ src/courses/index.ts
@@ -11,7 +11,8 @@ type VideoGroup = {
 };
 
 // To add new courses, add an import statement to the end of this block,
-// then add the course to the `RAW_COURSES` array below.
+// then add the course to the export `RAW_COURSES` array below.
+
 import introCS from "./introduction-to-cs-and-programming-using-python.json";
 import linearAlg from "./linear-algebra.json";
 import creole from "./creole-languages-and-caribbean-identities.json";
@@ -19,4 +20,5 @@ import introCS2 from "./introduction-to-computer-science-and-programming-in-pyth
 import singlevariablecalculus from "./single-variable-calculus.json";
 
 // This is the order courses will be displayed in the UI
+
 export const RAW_COURSES: RawCourse[] = [introCS, linearAlg, introCS2, creole, singlevariablecalculus];
