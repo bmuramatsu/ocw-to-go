96925f4bd914a80f2fcd15635372df2e91877ad5
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-06-17 15:15:48 -0600

    make it handle any level of bad ./static_resources links

--- src/course/fix_file_downloads.ts
+++ src/course/fix_file_downloads.ts
@@ -3,16 +3,31 @@
 // worker recognizes to force a download instead of opening the PDF in the browser, which doesn't
 // work reliably in all browsers.
 export default function fixFileDownloads() {
+  fixBadPaths();
+  fixDownloadButtons();
+}
+
+function fixBadPaths() {
   const badLinks = document.querySelectorAll("a[href^='./static_resources']");
+
+  // pathname will be something like /courses/6-100l-introduction-to-cs-and-programming-using-python-fall-2022/download/index.html
+  const slashes = (location.pathname.match(/\//g) || []).length;
+  const parents = slashes - 3;
+  if (parents < 0) return;
+
+  const prefix = "../".repeat(parents);
 
   badLinks.forEach((button) => {
     let href = button.getAttribute("href")!;
-    href = href.replace(/^\.\//, "../../");
+    href = href.replace(/^\.\//, prefix);
     button.setAttribute("href", href);
   });
+}
 
-
-  const pdfDownloads = document.querySelectorAll("a[href*='/static_resources/']");
+function fixDownloadButtons() {
+  const pdfDownloads = document.querySelectorAll(
+    "a[href*='/static_resources/']",
+  );
   pdfDownloads.forEach((button) => {
     let href = button.getAttribute("href")!;
     href = href + "?forcedownload=true";
