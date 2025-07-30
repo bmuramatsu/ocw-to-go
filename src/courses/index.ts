09f374ce60abcce65c3dcccac3840fc4ee0bb74e
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-07-30 17:26:42 -0600

    add new courses

--- src/courses/index.ts
+++ src/courses/index.ts
@@ -8,6 +8,61 @@ import course1 from "./18-06sc-linear-algebra-fall-2011.json";
 import course2 from "./24-908-creole-languages-and-caribbean-identities-spring-2017.json";
 import course3 from "./6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016.json";
 import course4 from "./18-01sc-single-variable-calculus-fall-2010.json";
+import course5 from "./1-89-environmental-microbiology-fall-2004.json";
+import course6 from "./6-092-introduction-to-programming-in-java-january-iap-2010.json";
+import course7 from "./18-05-introduction-to-probability-and-statistics-spring-2022.json";
+import course8 from "./6-0002-introduction-to-computational-thinking-and-data-science-fall-2016.json";
+import course9 from "./6-006-introduction-to-algorithms-spring-2020.json";
+import course10 from "./8-04-quantum-physics-i-spring-2016.json";
+import course11 from "./6-034-artificial-intelligence-fall-2010.json";
+import course12 from "./6-s096-introduction-to-c-and-c-january-iap-2013.json";
+import course13 from "./9-00sc-introduction-to-psychology-fall-2011.json";
+import course14 from "./18-700-linear-algebra-fall-2013.json";
+import course15 from "./8-02-physics-ii-electricity-and-magnetism-spring-2007.json";
+import course16 from "./6-033-computer-system-engineering-spring-2018.json";
+import course17 from "./18-657-mathematics-of-machine-learning-fall-2015.json";
+import course18 from "./18-02sc-multivariable-calculus-fall-2010.json";
+import course19 from "./6-042j-mathematics-for-computer-science-fall-2010.json";
+import course20 from "./18-s096-topics-in-mathematics-with-applications-in-finance-fall-2013.json";
+import course21 from "./res-6-007-signals-and-systems-spring-2011.json";
+import course22 from "./6-002-circuits-and-electronics-spring-2007.json";
+import course23 from "./14-01-principles-of-microeconomics-fall-2023.json";
+import course24 from "./15-401-finance-theory-i-fall-2008.json";
+import course25 from "./11-521-spatial-database-management-and-advanced-geographic-information-systems-spring-2003.json";
+import course26 from "./6-857-network-and-computer-security-spring-2014.json";
+import course27 from "./15-390-new-enterprises-spring-2013.json";
+import course28 from "./8-01sc-classical-mechanics-fall-2016.json";
+import course29 from "./res-6-012-introduction-to-probability-spring-2018.json";
+import course30 from "./15-s50-poker-theory-and-analytics-january-iap-2015.json";
+import course31 from "./18-03sc-differential-equations-fall-2011.json";
+import course32 from "./8-03sc-physics-iii-vibrations-and-waves-fall-2016.json";
+import course33 from "./5-111sc-principles-of-chemical-science-fall-2014.json";
+import course34 from "./3-091sc-introduction-to-solid-state-chemistry-fall-2010.json";
+import course35 from "./6-01sc-introduction-to-electrical-engineering-and-computer-science-i-spring-2011.json";
+import course36 from "./2-003sc-engineering-dynamics-fall-2011.json";
+import course37 from "./6-622-power-electronics-spring-2023.json";
+import course38 from "./7-016-introductory-biology-fall-2018.json";
+import course39 from "./18-100a-real-analysis-fall-2020.json";
+import course40 from "./9-13-the-human-brain-spring-2019.json";
+import course41 from "./22-01-introduction-to-nuclear-engineering-and-ionizing-radiation-fall-2016.json";
+import course42 from "./18-065-matrix-methods-in-data-analysis-signal-processing-and-machine-learning-spring-2018.json";
+import course43 from "./15-s12-blockchain-and-money-fall-2018.json";
+import course44 from "./15-s08-fintech-shaping-the-financial-world-spring-2020.json";
+import course45 from "./introduction-to-r-and-gis-fall-2023.json";
+import course46 from "./ec-701j-d-lab-i-development-fall-2009.json";
+import course47 from "./ec-720j-d-lab-ii-design-spring-2010.json";
+import course48 from "./ec-716-d-lab-waste-fall-2015.json";
+import course49 from "./21l-011-the-film-experience-fall-2013.json";
+import course50 from "./cms-611j-creating-video-games-fall-2014.json";
+import course51 from "./sts-042-einstein-oppenheimer-feynman-physics-in-the-20th-century-fall-2020.json";
+import course52 from "./24-900-introduction-to-linguistics-spring-2022.json";
+import course53 from "./17-41-introduction-to-international-relations-spring-2023.json";
+import course54 from "./21g-223-listening-speaking-and-pronunciation-fall-2004.json";
+import course55 from "./24-912-black-matters-introduction-to-black-studies-spring-2017.json";
+import course56 from "./cms-608-game-design-spring-2014.json";
+import course57 from "./17-50-introduction-to-comparative-politics-fall-2022.json";
+import course58 from "./21h-001-how-to-stage-a-revolution-fall-2013.json";
+import course59 from "./sts-003-the-rise-of-modern-science-fall-2010.json";
 
 export const RAW_COURSES: RawCourse[] = [
   course0,
@@ -15,4 +70,59 @@ export const RAW_COURSES: RawCourse[] = [
   course2,
   course3,
   course4,
+  course5,
+  course6,
+  course7,
+  course8,
+  course9,
+  course10,
+  course11,
+  course12,
+  course13,
+  course14,
+  course15,
+  course16,
+  course17,
+  course18,
+  course19,
+  course20,
+  course21,
+  course22,
+  course23,
+  course24,
+  course25,
+  course26,
+  course27,
+  course28,
+  course29,
+  course30,
+  course31,
+  course32,
+  course33,
+  course34,
+  course35,
+  course36,
+  course37,
+  course38,
+  course39,
+  course40,
+  course41,
+  course42,
+  course43,
+  course44,
+  course45,
+  course46,
+  course47,
+  course48,
+  course49,
+  course50,
+  course51,
+  course52,
+  course53,
+  course54,
+  course55,
+  course56,
+  course57,
+  course58,
+  course59,
 ];
