e80983309a986a82413d94186802adee9ed9d8c5
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-08-07 16:29:28 -0600

    add storage size everywhere. update version number

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,6 +3,7 @@ import { Link } from "wouter";
 import { Menu, Cancel } from "./svgs";
 import { ALL_COURSES } from "./initial_course_list";
 import useAutoCloseMenu from "./use_auto_close_menu";
+import StorageUsage from "./storage_usage";
 
 export default function Header() {
   const courseCount = React.useMemo(() => {
@@ -31,19 +32,42 @@ export default function Header() {
             <a href="https://ocw.mit.edu/about/">About OCW</a>
           </nav>
           <nav className="mobile-nav">
-            <button className="icon-btn icon-btn--clear" onClick={open}><Menu /></button>
+            <button className="icon-btn icon-btn--clear" onClick={open}>
+              <Menu />
+            </button>
             <div className={`mobile-nav__drawer ${navOpen ? "is-open" : ""}`}>
               <div className="mobile-nav__drawer-top">
                 <h2>Menu</h2>
-                <button className="icon-btn icon-btn--clear is-black" onClick={close}><Cancel /></button>
+                <button
+                  className="icon-btn icon-btn--clear is-black"
+                  onClick={close}
+                >
+                  <Cancel />
+                </button>
               </div>
               <ul>
-                <li><Link href="/" className={activeClass}>Home</Link></li>
-                <li><Link href="/all-courses" className={activeClass}>All courses ({courseCount})</Link></li>
-                <li><Link href="/my-courses" className={activeClass}>My courses</Link></li>
-                <li><a href="https://ocw.mit.edu/about/">About OCW</a></li>
+                <li>
+                  <Link href="/" className={activeClass}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/all-courses" className={activeClass}>
+                    All courses ({courseCount})
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/my-courses" className={activeClass}>
+                    My courses
+                  </Link>
+                </li>
+                <li>
+                  <a href="https://ocw.mit.edu/about/">About OCW</a>
+                </li>
               </ul>
-              <p className="mobile-nav__drawer-bottom">Local Storage Used: <span>0.6% (98 MB / 64 GB)</span></p>
+              <p className="mobile-nav__drawer-bottom">
+                <StorageUsage />
+              </p>
             </div>
           </nav>
         </header>
