82a63eeb380f5c8f0b73aa112eba0e033386754d
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-10-01 11:00:52 -0600

    add postcss for more backwards-compatible css

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,9 +1,22 @@
 import esbuild from "esbuild";
+// We're using this for postcss, but it can do SASS and others
+import stylePlugin from "esbuild-style-plugin";
+// This adds reasonable defaults for backwards compatibility with older browsers
+import postCssEnv from "postcss-preset-env";
+
 const [env] = process.argv.slice(2);
 
 const config = {
-  entryPoints: ["src/app.tsx", "src/course.ts", "src/worker.ts"],
+  entryPoints: [
+    "src/app.tsx",
+    "src/course.ts",
+    "src/worker.ts",
+    "src/styles.css",
+    "src/course-styles.css",
+    "src/video-downloader-styles.css",
+  ],
   bundle: true,
+  plugins: [stylePlugin({ postcss: { plugins: [postCssEnv()] } })],
   minify: false,
   sourcemap: true,
   target: "es2017",
