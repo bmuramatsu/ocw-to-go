89cb6c742f3e4a82745bd801b4c81faaff218bb2
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-10-06 17:58:30 -0600

    describe the new build process

--- build.mjs
+++ build.mjs
@@ -1,8 +1,22 @@
 import esbuild from "esbuild";
 import fs from "fs";
 
+// We use esbuild to bundle the js and css files. The build process is
+// complicated by the fact that we add hash suffixes to assets (to prevent
+// caching issues). Therefore, the index.html file and scripts cannot refer to
+// fixed filenames. This manifest needs to 1) replace paths in the index.html
+// file 2) Add the full manifest as json into the index.html so the app can look
+// it up and 3) inject the manifest as a constant into the web worker script.
+
+// In dev mode, the manifest is static, since we aren't worried about caching
+// there. In prod mode, the build must be done in multiple phases. First we
+// generate the main app file and css files, generate the manifest from that,
+// rewrite index.html, and then build the worker with the manifest
+
 const [env] = process.argv.slice(2);
 
+// default list does not include the worker because it must be built last in
+// prod mode
 const entryPoints = [
   "src/app.tsx",
   "src/course.ts",
@@ -35,6 +49,7 @@ async function build() {
     makeIndexHtml(DevManifest);
     copyStaticFiles();
 
+    // In dev, build the worker and assets together with a static manifest
     const ctx = await esbuild.context({
       ...config,
       entryPoints: [...entryPoints, "src/worker.ts"],
@@ -81,6 +96,7 @@ async function build() {
   }
 }
 
+// We serve the Dist file. It's ignored by git and cleared out with each build
 function resetDist() {
   if (fs.existsSync("dist")) {
     fs.rmSync("dist", { recursive: true });
@@ -88,6 +104,7 @@ function resetDist() {
   fs.mkdirSync("dist");
 }
 
+// This does very simple string replacements to the correct file paths
 function makeIndexHtml(manifest) {
   let index = fs.readFileSync("src/index.html", "utf8");
   index = index.replace("__APP_JS__", manifest["/app.tsx"]);
@@ -96,6 +113,7 @@ function makeIndexHtml(manifest) {
   fs.writeFileSync("dist/index.html", index);
 }
 
+// All static assets should go in the static folder. They will be erased from dist
 function copyStaticFiles() {
   fs.cpSync("static", "dist", { recursive: true });
 }
