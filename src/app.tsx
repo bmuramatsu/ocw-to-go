ee6fa4592050b371c8246bb2f00c34bfb20e7913
Ryker Blunck <ryker.blunck@atomicjolt.com>
2025-10-15 17:11:05 -0600

    stop activating the worker in 2 places. This was undoing an important optimization

--- src/app.tsx
+++ src/app.tsx
@@ -3,17 +3,7 @@ import React from "react";
 import Root from "./app/root";
 
 async function init() {
-  await activateWorker();
   createRoot(document.getElementById("react-app")!).render(<Root />);
-}
-
-function activateWorker() {
-  return new Promise<ServiceWorker>((resolve) => {
-    navigator.serviceWorker.register("/worker.js");
-    navigator.serviceWorker.ready.then((registration) => {
-      resolve(registration.active!);
-    });
-  });
 }
 
 init();
