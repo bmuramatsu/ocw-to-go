21a9d3ae0b8ad740480880208ca125c7b4634a09
Brandon Muramatsu <752326+bmuramatsu@users.noreply.github.com>
2025-11-13 07:26:08 +1300

    Rename ocwtogo-logo.svg to to-go-logo.svg

--- src/worker/assets.ts
+++ src/worker/assets.ts
@@ -18,7 +18,7 @@ const ASSETS_TO_CACHE = [
   "/images/instagram-icon.png",
   "/images/x-icon.png",
   "/images/youtube-icon.png",
-  "/images/ocwtogo-logo.svg",
+  "/images/to-go-logo.svg",
   "/images/oeglobal.png",
   "/images/linkedin-icon.png",
   "/images/mit-logo.svg",
